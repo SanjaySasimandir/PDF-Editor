f6c16c9bde3c0edd47572df639bc3a821fe343e0
Sanjay S <SanjaySasimandir@users.noreply.github.com>
2023-12-15 17:37:00 +0530

    cleanup

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,39 +1,45 @@
-import { ChangeEvent, useEffect, useRef, useState } from "react";
+import React, {
+  ChangeEvent,
+  createContext,
+  useEffect,
+  useRef,
+  useState,
+} from "react";
 import { Document, Page } from "react-pdf";
 import "./App.css";
 import { pdfjs } from "react-pdf";
 import Axios from "axios";
+import { Editor } from "./components/editor";
 
 pdfjs.GlobalWorkerOptions.workerSrc = new URL(
   "pdfjs-dist/build/pdf.worker.min.js",
   import.meta.url
 ).toString();
 
-function App() {
-  // Code for  dynamic resize of pdf page starts
-  const myDivRef = useRef<HTMLDivElement | null>(null);
-  const [pageWidth, setPageWidth] = useState(0);
-  const getRefWidth = () => {
-    if (myDivRef.current) {
-      const width = myDivRef.current.getBoundingClientRect().width;
-      setPageWidth(width);
-    }
-  };
-  useEffect(() => {
-    getRefWidth();
-    window.addEventListener("resize", getRefWidth);
-    return () => {
-      window.removeEventListener("resize", getRefWidth);
-    };
-  }, []);
-  // Code for  dynamic resize of pdf page ends
+export const EditorContext = createContext({
+  selectedFile: null as File | null,
+  setSelectedFile: (file: File | null) => {},
+  totalPages: 1,
+  setTotalPages: (number: number) => {},
+  pageOrder: [] as number[],
+  setPageOrder: (numbers: number[]) => {},
+  movePageUp: (pageNumber: number) => {},
+  movePageDown: (pageNumber: number) => {},
+  deletePage: (index: number) => {},
+});
 
+function App() {
   const [selectedFile, setSelectedFile] = useState<File | null>(null);
   const [totalPages, setTotalPages] = useState<number>(1);
   const [pageOrder, setPageOrder] = useState<number[]>(
     Array.from({ length: totalPages }, (_, index) => index + 1)
   );
+  const [newPDFName, setNewPDFName] = useState("");
 
+  const inputRef = useRef<HTMLInputElement | null>(null);
+  const handleUploadClick = () => {
+    inputRef.current?.click();
+  };
   const handleFileSelection = (event: ChangeEvent<HTMLInputElement>) => {
     const file = event.target.files?.[0];
     if (file && file.type === "application/pdf") {
@@ -41,19 +47,26 @@ function App() {
     }
   };
 
-  const handleDocLoadSuccess = ({ numPages }: { numPages: number }) => {
-    setTotalPages(numPages);
-    setPageOrder(Array.from({ length: numPages }, (_, index) => index + 1));
-    getRefWidth();
+  const resetPDF = () => {
+    setPageOrder(Array.from({ length: totalPages }, (_, index) => index + 1));
+    setNewPDFName("");
   };
 
-  const inputRef = useRef<HTMLInputElement | null>(null);
-  const handleUploadClick = () => {
-    inputRef.current?.click();
+  const handleFileUpload = () => {
+    const fd = new FormData();
+    if (selectedFile && pageOrder) {
+      fd.append("file", selectedFile);
+      fd.append("pageOrder", JSON.stringify(pageOrder));
+    }
+    Axios.post("http://localhost:4000/upload", fd)
+      .then((res) => {
+        setNewPDFName(res.data.file);
+      })
+      .catch((err) => console.log(err));
   };
 
   const movePageUp = (pageNumber: number) => {
-    setPageOrder((prevPageOrder) => {
+    setPageOrder((prevPageOrder: any) => {
       const index = prevPageOrder.indexOf(pageNumber);
       if (index > 0) {
         let newPageOrder = [...prevPageOrder];
@@ -68,7 +81,7 @@ function App() {
   };
 
   const movePageDown = (pageNumber: number) => {
-    setPageOrder((prevPageOrder) => {
+    setPageOrder((prevPageOrder: any) => {
       const index = prevPageOrder.indexOf(pageNumber);
       if (index < prevPageOrder.length - 1) {
         let newPageOrder = [...prevPageOrder];
@@ -83,45 +96,37 @@ function App() {
   };
 
   const deletePage = (index: number) => {
-    setPageOrder((prevPageOrder) => {
+    setPageOrder((prevPageOrder: any) => {
       let newPageOrder = [...prevPageOrder];
       newPageOrder.splice(index, 1);
       return newPageOrder;
     });
   };
 
-  const resetPDF = () => {
-    setPageOrder(Array.from({ length: totalPages }, (_, index) => index + 1));
-  };
-
-  const [newPDFName, setNewPDFName] = useState("");
-  const handleFileUpload = () => {
-    const fd = new FormData();
-    if (selectedFile && pageOrder) {
-      fd.append("file", selectedFile);
-      fd.append("pageOrder", JSON.stringify(pageOrder));
-    }
-    Axios.post("http://localhost:4000/upload", fd)
-      .then((res) => {
-        console.log(res.data);
-        setNewPDFName(res.data.file);
-      })
-      .catch((err) => console.log(err));
+  const unselectFile = () => {
+    setSelectedFile(null);
   };
 
   return (
-    <div className="App bg-gray-800">
+    // <div className="App bg-[#0F0F0F] min-h-screen">
+    <div className="App bg-[#1d1d1f] min-h-screen">
       <link
         rel="stylesheet"
         href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200"
       />
-      <button
-        className="px-4 py-2 font-semibold text-sm bg-cyan-500 text-white rounded-full shadow-sm hover:bg-cyan-600 
+
+      {!!!selectedFile && (
+        <div className="flex items-center justify-center h-screen">
+          <button
+            className="px-4 py-2 font-semibold text-sm bg-cyan-500 text-white rounded-full shadow-sm hover:bg-cyan-600 
         active:bg-cyan-700 focus:outline-none focus:ring focus:ring-cyan-300"
-        onClick={handleUploadClick}
-      >
-        Upload
-      </button>
+            onClick={handleUploadClick}
+          >
+            Upload
+          </button>
+        </div>
+      )}
+
       <input
         className="hidden"
         type="file"
@@ -129,82 +134,63 @@ function App() {
         onChange={handleFileSelection}
         ref={inputRef}
       />
-      <button
-        className="bg-red-900 rounded-full text-white font-semibold text-sm hover:bg-red-800 px-4 py-2"
-        onClick={resetPDF}
-      >
-        Reset
-      </button>
-      <button
-        className="bg-blue-900 rounded-full text-white font-semibold text-sm hover:bg-blue-800 px-4 py-2"
-        onClick={handleFileUpload}
-      >
-        Get Download Link
-      </button>
-      <a
-        className="bg-indigo-900 rounded-full text-white font-semibold text-sm hover:bg-indigo-800 px-4 py-2"
-        href={"http://localhost:4000/download/" + newPDFName}
-        download
-      >
-        Download PDF
-      </a>
-      <Document
-        className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4"
-        file={selectedFile}
-        onLoadSuccess={handleDocLoadSuccess}
-      >
-        {pageOrder.map((pageNumber, index) => (
-          <div
-            className="border-solid border-4 border-sky-500 m-2 rounded-md"
-            key={pageNumber}
-            ref={index == 0 ? myDivRef : null}
-            style={{ position: "relative", zIndex: "auto" }}
-          >
-            <div className="overlay absolute z-40 bg-blend-overlay bg-black/50 w-full flex justify-end">
-              {pageNumber}
-              {index !== 0 && (
-                <button
-                  className="bg-violet-500 hover:bg-violet-600 active:bg-violet-700 focus:outline-none focus:ring focus:ring-violet-300 rounded-full m-1"
-                  onClick={() => movePageUp(pageNumber)}
-                >
-                  <span className="material-symbols-outlined">
-                    arrow_upward
-                  </span>
-                </button>
-              )}
-              {index !== totalPages - 1 && (
-                <button
-                  className="bg-violet-500 hover:bg-violet-600 active:bg-violet-700 focus:outline-none focus:ring focus:ring-violet-300 rounded-full m-1"
-                  onClick={() => movePageDown(pageNumber)}
-                >
-                  <span className="material-symbols-outlined">
-                    arrow_downward
-                  </span>
-                </button>
-              )}
+
+      {selectedFile && (
+        <div>
+          <div className="md:container mx-auto flex justify-between px-2 pt-4">
+            <button
+              className="font-semibold text-[#0071e3] text-sm hover:bg-[red-800] px-2"
+              onClick={unselectFile}
+            >
+              <span className="material-symbols-outlined">arrow_back</span>
+            </button>
+            <div className="space-x-4">
               <button
-                className="bg-red-500 hover:bg-red-600 active:bg-red-700 focus:outline-none focus:ring focus:ring-red-300 rounded-full m-1"
-                onClick={() => deletePage(index)}
+                className="underline rounded-full text-[#0071e3] font-semibold text-sm px-2 py-2 hover:bg-white hover:px-2"
+                onClick={resetPDF}
               >
-                <span className="material-symbols-outlined">delete</span>
+                Reset
               </button>
+              {!newPDFName && (
+                <button
+                  className="bg-[#0071e3] rounded-full text-white font-semibold text-sm hover:bg-blue-800 px-4 py-2 disabled:opacity-50"
+                  onClick={handleFileUpload} disabled={pageOrder.length===0}
+                >
+                  Get Download Link
+                </button>
+              )}
+              {newPDFName && (
+                <a
+                  className="bg-indigo-900 rounded-full text-white font-semibold text-sm hover:bg-indigo-800 px-4 py-2"
+                  href={"http://localhost:4000/download/" + newPDFName}
+                  download
+                >
+                  Download PDF
+                </a>
+              )}
             </div>
-            <Page
-              pageNumber={pageNumber}
-              width={pageWidth - 8}
-              renderTextLayer={false}
-              renderAnnotationLayer={false}
-            ></Page>
           </div>
-        ))}
-      </Document>
-      <button
-        onClick={() => {
-          console.log(pageOrder);
-        }}
-      >
-        order
-      </button>
+
+          {/* <div className="bg-[#232D3F] md:container mx-auto md:px-2 md:py-2 mt-4 rounded-lg"> */}
+          <div className="bg-[#141414] md:container mx-auto md:px-2 md:py-2 mt-4 rounded-lg">
+            <EditorContext.Provider
+              value={{
+                selectedFile,
+                setSelectedFile,
+                totalPages,
+                setTotalPages,
+                pageOrder,
+                setPageOrder,
+                movePageUp,
+                movePageDown,
+                deletePage,
+              }}
+            >
+              <Editor></Editor>
+            </EditorContext.Provider>
+          </div>
+        </div>
+      )}
     </div>
   );
 }
--- frontend/src/components/editor.tsx
+++ frontend/src/components/editor.tsx
@@ -1,3 +1,93 @@
+import { useContext, useEffect, useRef, useState } from "react";
+import { Document, Page } from "react-pdf";
+import { EditorContext } from "../App";
+
 export function Editor() {
-    return (<div></div>)
+  const {
+    selectedFile,
+    setSelectedFile,
+    totalPages,
+    setTotalPages,
+    pageOrder,
+    setPageOrder,
+    movePageUp,
+    movePageDown,
+    deletePage,
+  } = useContext(EditorContext);
+
+  // Code for  dynamic resize of pdf page starts
+  const myDivRef = useRef<HTMLDivElement | null>(null);
+  const [pageWidth, setPageWidth] = useState(0);
+
+  useEffect(() => {
+    getRefWidth();
+    window.addEventListener("resize", getRefWidth);
+    return () => {
+      window.removeEventListener("resize", getRefWidth);
+    };
+  }, []);
+  const getRefWidth = () => {
+    if (myDivRef.current) {
+      const width = myDivRef.current.getBoundingClientRect().width;
+      setPageWidth(width);
+    }
+  };
+  // Code for  dynamic resize of pdf page ends
+
+  const handleDocLoadSuccess = ({ numPages }: { numPages: number }) => {
+    setTotalPages(numPages);
+    setPageOrder(Array.from({ length: numPages }, (_, index) => index + 1));
+    getRefWidth();
+  };
+
+  return (
+    <Document
+      className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4"
+      file={selectedFile}
+      onLoadSuccess={handleDocLoadSuccess}
+    >
+      {pageOrder.map((pageNumber, index) => (
+        <div
+          className="border-solid border-4 border-[#0071e3] m-2 rounded-md"
+          key={pageNumber}
+          ref={index == 0 ? myDivRef : null}
+          style={{ position: "relative", zIndex: "auto" }}
+        >
+          <div className="overlay absolute z-40 bg-blend-overlay bg-black/50 w-full flex justify-end">
+            {pageNumber}
+            {index !== 0 && (
+              <button
+                className="bg-violet-500 text-white hover:bg-violet-600 active:bg-violet-700 focus:outline-none focus:ring focus:ring-violet-300 rounded-full m-1"
+                onClick={() => movePageUp(pageNumber)}
+              >
+                <span className="material-symbols-outlined">arrow_upward</span>
+              </button>
+            )}
+            {index !== totalPages - 1 && (
+              <button
+                className="bg-violet-500 text-white hover:bg-violet-600 active:bg-violet-700 focus:outline-none focus:ring focus:ring-violet-300 rounded-full m-1"
+                onClick={() => movePageDown(pageNumber)}
+              >
+                <span className="material-symbols-outlined">
+                  arrow_downward
+                </span>
+              </button>
+            )}
+            <button
+              className="bg-red-500 hover:bg-red-600 text-white active:bg-red-700 focus:outline-none focus:ring focus:ring-red-300 rounded-full m-1"
+              onClick={() => deletePage(index)}
+            >
+              <span className="material-symbols-outlined mt-[5px]">delete</span>
+            </button>
+          </div>
+          <Page
+            pageNumber={pageNumber}
+            width={pageWidth - 8}
+            renderTextLayer={false}
+            renderAnnotationLayer={false}
+          ></Page>
+        </div>
+      ))}
+    </Document>
+  );
 }
